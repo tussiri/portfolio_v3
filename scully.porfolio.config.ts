14a6c8a17e9c95af5e789471b22a8029b7315676
Tumaini Ussiri <40671546+tussiri@users.noreply.github.com>
2023-03-28 15:34:31 -0500

    fixed typo for the build file, and changed the webpage title

--- scully.porfolio.config.ts
+++ scully.porfolio.config.ts
@@ -1,7 +1,7 @@
 import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: "./src",
-  projectName: "porfolio",
+  projectName: "portfolio",
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'porfolio';
+  title = 'portfolio';
 }
